7466dc42155fad9d27742ddfb779f023ed3c6c06
kkellum <kkellum109@gmail.com>
2019-07-26 15:29:16 -0700

    camera app initial commit update checkpoint 7

--- app/src/main/java/com/kkellum/cameraapp/MainActivity.java
+++ app/src/main/java/com/kkellum/cameraapp/MainActivity.java
@@ -1,16 +1,19 @@
 package com.kkellum.cameraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Notification;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     String currentPhotoPath;
     private Notification.Action.Builder data;
+    private Uri mCurrentPhotoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mcamera_button.setOnClickListener(this);
 
-        private File createImageFile() throws IOException {
+        private File createImageFile()throws IOException {
             // Create an image file name
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             String imageFileName = "JPEG_" + timeStamp + "_";
@@ -57,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             currentPhotoPath = image.getAbsolutePath();
             return image;
         }
-        private void dispatchTakePictureIntent() {
+        private void dispatchTakePictureIntent(); {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -79,6 +83,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
     }
+
+    public MainActivity(ImageView mimage_view) {
+        this.mimage_view = mimage_view;
+    }
+
+    private void dispatchSharePicture()
+    {
+        File image = new File(mCurrentPhotoUri.getPath());
+        Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", image);
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, photoURI);
+        shareIntent.setType("image/jpeg");
+
+        startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.share_pic)));
+
+        // Create the text message with a string
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        boolean textMessage = false;
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage);
+        sendIntent.setType("text/plain");
+
+// Verify that the intent will resolve to an activity
+        if (sendIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(sendIntent);
+        }
+
+    }
+
+
+
+    private void dispatchEmailPicture()
+    {
+        File image = new File(mCurrentPhotoUri.getPath());
+        Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", image);
+
+        Intent emailIntent = new Intent();
+        emailIntent.setAction(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Check out my pic!");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Taken using my CameraApp.");
+        emailIntent.putExtra(Intent.EXTRA_STREAM, photoURI);
+
+        if (emailIntent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(emailIntent);
+        }
+        else
+        {
+            Toast.makeText(this, "No email app configured.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
+
 
 
